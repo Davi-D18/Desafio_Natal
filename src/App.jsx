65767820f0e57e2f5f16fb28c4d47f3c0744fd07
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-12-26 15:50:23 -0300

    README adicionado e pequenos ajustes no código feito

--- src/App.jsx
+++ src/App.jsx
@@ -13,37 +13,44 @@ export function App() {
 
   return (
     <>
-     <Header />
+      <Header />
 
-     <main>
-      <section className={S.banner} id="inicio">
-        <div>
-          <Snowfall 
-            color="#fff"
-            snowflakeCount={100}
-          />
-        </div>,
-      </section>
+      <main>
+        <section className={S.banner} id="inicio">
+          <div>
+            <Snowfall color="#fff" snowflakeCount={110} />
+          </div>
+          ,
+        </section>
 
-      <section className={S.curiosidade}>
-        <div className={S.containerCuriosidade}>
-          <img src={arvoreBola} alt="Imagem de uma arvore dentro de uma bola" />   
+        <section className={S.curiosidade}>
+          <div className={S.containerCuriosidade}>
+            <img
+              src={arvoreBola}
+              alt="Imagem de uma arvore dentro de uma bola"
+            />
 
-          <p id="curiosidade-scrool">A origem do globo de neve é incerta, mas acredita-se que tenha surgido na Europa no século XIX, tornando-se um símbolo de magia, afeto e encantamento, especialmente no Natal, trazendo um toque de nostalgia e maravilha para as celebrações e encantando gerações ao redor do mundo.</p>
-        </div>
-      </section>
+            <p id="curiosidade-scrool">
+              A origem do globo de neve é incerta, mas acredita-se que tenha
+              surgido na Europa no século XIX, tornando-se um símbolo de magia,
+              afeto e encantamento, especialmente no Natal, trazendo um toque de
+              nostalgia e maravilha para as celebrações e encantando gerações ao
+              redor do mundo.
+            </p>
+          </div>
+        </section>
 
-      <section className={S.gallery} id="galeria">
-        <div className={S.galleryContainer}>
-          <img src={Image1} alt="Cidade para viajar no natal" />
-          <img src={Image2} alt="Cidade para viajar no natal" />
-          <img src={Image3} alt="Cidade para viajar no natal" />
-        </div>
-      </section>
-     </main>
+        <section className={S.gallery} id="galeria">
+          <div className={S.galleryContainer}>
+            <img src={Image1} alt="Cidade para viajar no natal" />
+            <img src={Image2} alt="Cidade para viajar no natal" />
+            <img src={Image3} alt="Cidade para viajar no natal" />
+          </div>
+        </section>
+      </main>
 
-     <Footer />
+      <Footer />
     </>
-  )
+  );
 }
 
